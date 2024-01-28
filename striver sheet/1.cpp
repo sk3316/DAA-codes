9fd16fa1f26192df95d0713bb4d9f71584511e94
sk3316 <sk3316@srmist.edu.in>
2024-01-29 00:18:43 +0530

    .

--- striver sheet/1.cpp
+++ striver sheet/1.cpp
@@ -2,25 +2,28 @@
 using namespace std;
 int main()
 {
-    cout<<"namaste duniya"<<endl;
-    int a = 5;
-    cout<<a<<endl;
-    char ch = 'c';
-    cout<<ch<<endl;
-    bool bl = true;
-    cout<<bl<<endl;
-    float f= 1.2;
-    cout<<f<<endl;
-    double d = 1.23;
-    cout<<d<<endl;
+    // cout<<"namaste duniya"<<endl;
+    // int a = 5;
+    // cout<<a<<endl;
+    // char ch = 'c';
+    // cout<<ch<<endl;
+    // bool bl = true;
+    // cout<<bl<<endl;
+    // float f= 1.2;
+    // cout<<f<<endl;
+    // double d = 1.23;
+    // cout<<d<<endl;
 
-    int size = sizeof(f);
-    cout<<"size of a is:"<<size<<endl;
+    // int size = sizeof(f);
+    // cout<<"size of a is:"<<size<<endl;
 
-    int z = 'a';
-    cout<<z<<endl;
-    char ch1 = 9823456;
-    cout<<ch1<<endl;
+    // int z = 'a';
+    // cout<<z<<endl;
+    // char ch1 = 9823456;
+    // cout<<ch1<<endl;
 
+
+    int ab = '9';
+    cout<<ab;
     return 0;
 }
--- striver sheet/2.cpp
+++ striver sheet/2.cpp
@@ -2,19 +2,95 @@
 using namespace std;
 int main()
 {
-    int a, b;
-    cin >> a >> b;
-    if (a > b)
+    // int a, b;
+    // cin >> a >> b;
+    // if (a > b)
+    // {
+    //     cout << a << endl;
+    // }
+    // else if (b > a)
+    // {
+    //     cout << b << endl;
+    // }
+    // else
+    // {
+    //     cout << "both are equal" << endl;
+    // }
+
+    // char ch;
+    // cout << "enter the value of ch: " << endl;
+    // cin >> ch;
+    // if (ch >= 97 && ch <= 122)
+    // {
+    //     cout << "your entered value lies between the lower case alphabets." << endl;
+    // }
+    // else if (ch >= 65 && ch <= 90)
+    // {
+    //     cout << "your entered value lies between upper case alphabets. " << endl;
+    // }
+    // else if (ch >= 48 && ch <= 57)
+    // {
+    //     cout << "your entered value lies between 0 to 9." << endl;
+    // }
+    // else
+    // {
+    //     cout << "it is a symbol." << endl;
+    // }
+
+    // int i, n, sum = 0;
+    // cin >> i >> n;
+    // while (i <= n)
+    // {
+    //     sum += i;
+    //     cout << sum << endl;
+    //     // cout<<i<<endl;
+    //     i++;
+    // }
+
+    // int n, i = 2;
+    // cin >> n;
+    // while (i < n)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         cout << i << "it is composite." << endl;
+    //     }
+    //     else
+    //     {
+    //         cout << i << "it is a prime number." << endl;
+    //     }
+    //     i++;
+    // }
+
+    // int n, i = 1;
+    // cin >> n;
+    // while (i <= n)
+
+    // {
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         cout << "*";
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
     {
-        cout << a << endl;
+        int j = 1;
+        while (j <= n)
+        {
+            cout << i;
+            j++;
+        }
+        cout << endl;
+        i++;
     }
-    else if (b > a)
-    {
-        cout << b << endl;
-    }
-    else
-    {
-        cout << "both are equal" << endl;
-    }
+
     return 0;
 }
