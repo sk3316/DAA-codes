7bf48d6a9e1c93fd3b692332642562f312b4f27c
sk3316 <sk3316@srmist.edu.in>
2024-08-08 21:51:23 +0530

    commit;

--- love babbar/strings/strings.cpp
+++ love babbar/strings/strings.cpp
@@ -1,7 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    
+    string name;
+    cout<<"Enter your name: "<<endl;
+    cin>>name;
+    cout<<"Your name is: "<<name<<endl;
+    cout<<"Length: "<<name.length()<<endl;
+    reverse(name.begin(),name.end());
+    cout<<"Reversed name: "<<name<<endl;
     return 0;
 }
